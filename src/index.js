2b0e8d8e13b2bef135e795d3702e050526b04a8b
xia <x.l@apple.com>
2022-08-26 23:09:49 +0100

    changer ApolloClient uri to server uri

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-    uri: 'https://xia-client-catstronauts.herokuapp.com',
+    uri: 'https://xia-server-catstronauts.herokuapp.com/',
     cache: new InMemoryCache(),
 });
 
