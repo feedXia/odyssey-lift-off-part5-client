b82815b47de7d99adc07b3de94282e2fe10df149
xia <x.l@apple.com>
2022-08-26 23:17:58 +0100

    test with tutorial uri

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-    uri: 'https://xia-server-catstronauts.herokuapp.com/',
+    uri: 'https://server-catstronauts.herokuapp.com/',
     cache: new InMemoryCache(),
 });
 
